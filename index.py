4732eecdfffe7c832ed569bde77ca1ec34c4500b
alice2005921-dot <alice2005921@gmail.com>
2026-05-28 12:06:52 +0800

    5/28

--- index.py
+++ index.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 from flask import Flask, render_template, request, make_response, jsonify
 from datetime import datetime
 
-
+from google.genai import types
 import os
 import json
 import firebase_admin
@@ -129,7 +129,28 @@ def webhook():
             info += "目前資料庫中找不到符合此分級的電影喔！"
 
     elif (action == "input.unknown"):
-        info =  req["queryResult"]["queryText"]
+        instruction_text = (
+            "你是一個熱心且知識豐富的專業智慧助理。"
+            "對於使用者的提問，請回覆重點的關鍵字，不要重述問題。"         
+        )
+
+
+        ai_config = types.GenerateContentConfig(
+            max_output_tokens=500, 
+            system_instruction=instruction_text
+        )
+        response = client.models.generate_content(
+            model='gemini-3.5-flash', 
+            contents=req["queryResult"]["queryText"],
+            config=ai_config,
+        )
+
+        if response.text:
+            info = response.text
+        else:
+            info = "抱歉，我現在無法生成回應，請稍後再試。"
+
+
 
 
     # 將整理好的字串包裝成 Dialogflow 看得懂的 JSON 格式回傳
